c7bb2302ee1d01d4dc0645aca672940c279b0951
gkaradov <karadov9518@rogers.com>
2020-12-14 02:27:45 -0500

    bvcbcvb

---
+++ src/components/Header.tsx
@@ -0,0 +1,10 @@
+import React from "react"
+
+const header = ({ header }) => (
+  <div className="header">
+    <h1>{header.title}</h1>
+    <p>Geo Search</p>
+  </div>
+)
+
+export default article
---
+++ src/containers/Locations.tsx
@@ -0,0 +1,2 @@
+import React, { useState } from "react"
+import Location from "../components/Location/Location"
